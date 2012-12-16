8ac5de7e60837eaf6a95a815785e0fbcc5de3c33
Martijn Berger <martijn.berger@gmail.com>
2012-12-16 19:50:26 +0100

    make it build on OS X

--- setup.py
+++ setup.py
@@ -4,6 +4,22 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import platform
+
+if platform.system() == 'Linux':
+    libraries = ["maxwellsdk"]
+    extra_compile_args=['-D_LINUX']
+    extra_link_args=['-Llib']
+elif platform.system() == 'Darwin': # OS x
+    libraries=["mxs"]
+    extra_compile_args=['-D_MACOSX','-D_LINUX','-DCOMPILER_GCC']
+    extra_link_args=['-Llib/','-framework CoreGraphics']
+else:
+    libraries=["maxwellsdk"]
+    extra_compile_args=['-D_LINUX']
+    extra_link_args=['-Llib']
+
+
 setup(
       name = "Maxwell",
       cmdclass = {"build_ext": build_ext},
@@ -12,8 +28,8 @@ setup(
     ["maxwell.pyx"],           # filename of our Pyrex/Cython source
     language="c++",              # this causes Pyrex/Cython to create C++ source
     include_dirs=["h"],          # usual stuff
-    libraries=["maxwellsdk"],             # ditto
-    extra_compile_args=['-D_LINUX'],
-    extra_link_args=['-Llib']
+    libraries=libraries,             # ditto
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args
     )]
     )
